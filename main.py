b4f50644c82f0dd4ca003ac0fe4c5fce96871fb3
itsdoddsy <23368872+itsdoddsy@users.noreply.github.com>
2019-01-10 21:51:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+import requests
+import youtube_dl
+import os
+
+YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v='
+GOOGLE_API_URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={}&maxResults=3&order=date&type=video&key={}'
+API_KEY = ''
+
+PLAYLIST_LIST = json.load(open('channels.json', 'r'))
+
+__here__ = os.getcwd() + '\\'
+
+def DownloadVideo(url, folder):
+    ydl_options = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '128',
+        }],
+        'outtmpl': __here__ + folder + '\%(title)s.%(ext)s',
+        'prefer_ffmpeg': True,
+        'download_archive': 'downloadedsongs.txt'
+    }
+    with youtube_dl.YoutubeDL(ydl_options) as ydl:
+        ydl.extract_info(YOUTUBE_VIDEO_URL+url, download=True)
+
+
+for channel in PLAYLIST_LIST.values():
+    r = requests.get(GOOGLE_API_URL.format(channel[0]['url'], API_KEY))
+    recentsongs = json.loads(r.text)['items']
+    for songs in recentsongs:
+        DownloadVideo(songs['id']['videoId'], channel[0]['folder'])
